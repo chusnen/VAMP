5314324077dad8269ad3edc3b5e7530e077a9d51
Adolfo <adoltole@gmail.com>
2014-09-26 13:03:51 +0200

    ssh key added from local to vagrant correctly

--- up.sh
+++ up.sh
@@ -1,4 +1,4 @@
 mkdir /Volumes/tmp
+ssh-add
 vagrant up
-defaults write com.apple.finder ShowMountedServersOnDesktop 1
-/usr/local/bin/sshfs vagrant@192.168.44.44:/var/www/ /Volumes/tmp -ovolname=vagrant,reconnect -ocache=no -onolocalcaches 
+/usr/local/bin/sshfs vagrant@192.168.44.44:/var/www/ /Volumes/tmp -ovolname=vagrant,reconnect -ocache=no -onolocalcaches
